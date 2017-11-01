385344b02a33974ba141bb9b6b8c8b663828299b
Erik O'Leary <erik.m.oleary@gmail.com>
2017-10-31 22:39:30 -0500

    Added set-deploymentconfig cmdlet (#60)
    * Added set-deploymentconfig cmdlet
    
    * Simplified set-deployment
    
    - Fixed issue not waiting for publish to finish

---
+++ src/Etg.Yams.Powershell/SetDeploymentConfigCmdlet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Management.Automation;
+using Etg.Yams.Azure.Storage;
+using Etg.Yams.Storage.Config;
+
+namespace Etg.Yams.Powershell
+{
+    [Cmdlet(VerbsCommon.Set, "DeploymentConfig")]
+    [OutputType(typeof(DeploymentConfig))]
+    public class SetDeploymentConfigCmdlet : Cmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "The connection string of the Yams storage")]
+        public string ConnectionString { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "The deployment configuration to set")]
+        public DeploymentConfig Config { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(ConnectionString))
+                {
+                    throw new ArgumentException(nameof(ConnectionString));
+                }
+
+                int activityId = 0;
+                var progressRecord = new ProgressRecord(activityId++, "Connect to blob storage",
+                    "Connecting to blob storage");
+                WriteProgress(progressRecord);
+                var deploymentRepository = BlobStorageDeploymentRepository.Create(ConnectionString);
+                progressRecord.RecordType = ProgressRecordType.Completed;
+                WriteProgress(progressRecord);
+
+                progressRecord = new ProgressRecord(activityId++, "PublishDeploymentConfig",
+                    "Publishing DeploymentConfig.json to blob storage");
+                deploymentRepository.PublishDeploymentConfig(Config).Wait();
+                WriteProgress(progressRecord);
+
+                WriteObject(Config);
+            }
+            catch (Exception e)
+            {
+                ThrowTerminatingError(new ErrorRecord(e, "0", ErrorCategory.OperationStopped, null));
+            }
+        }
+    }
+}
