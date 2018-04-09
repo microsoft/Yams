4e6ae247f5b3af20d7b609f40bc1cac3da6f9731
Nehme Bilal <nbilal@microsoft.com>
2018-04-09 14:19:23 -0700

    Make sure monitored initialization is done before starting to monitor heart beats

--- src/Etg.Yams.Core/Process/ProcessFactory.cs
+++ src/Etg.Yams.Core/Process/ProcessFactory.cs
@@ -18,22 +18,22 @@ namespace Etg.Yams.Process
         {
             IProcess process = new Process(identity, exePath, _config.ShowApplicationProcessWindow);
 
+            if (monitorInitialization)
+            {
+                string pipeName = Guid.NewGuid().ToString();
+                process = new MonitorInitProcessDecorator(identity, _config, process,
+                    new IpcConnection(new NamedPipeServerAdapter(pipeName)));
+            }
             if (gracefulShutdown)
             {
                 string pipeName = Guid.NewGuid().ToString();
                 process = new GracefulShutdownProcessDecorator(identity, _config, process,
                     new IpcConnection(new NamedPipeServerAdapter(pipeName)));
             }
-            if (monitorInitialization)
-            {
-                string pipeName = Guid.NewGuid().ToString();
-                process = new HealthProcessDecorator(identity, _config, process,
-                    new IpcConnection(new NamedPipeServerAdapter(pipeName)));
-            }
             if (monitorHealth)
             {
                 string pipeName = Guid.NewGuid().ToString();
-                process = new MonitorInitProcessDecorator(identity, _config, process,
+                process = new HealthProcessDecorator(identity, _config, process,
                     new IpcConnection(new NamedPipeServerAdapter(pipeName)));
             }
             return new SelfRestartingProcess(process, _config.ApplicationRestartCount);
